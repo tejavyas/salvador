9221cc692100f32716280d670036fafa0a2b9a0f
tejavyas <128081186+tejavyas@users.noreply.github.com>
2025-02-19 16:28:03 +0000

    continued progress

--- server.js
+++ server.js
@@ -14,9 +14,29 @@ import cors from 'cors';
 // middleware is code that runs on every single request
 const app = express();
 // cross origin resource sharing
-app.use(cors());
 // tells api that it only wants to handle JSON data
+app.use(cors());
 app.use(express.json());
 
 // create first endpoint
-app.post('/jsProject1');
+// argument 1: url to navigate to
+// argument 2: async callback function
+app.post('/jsProject1', async (req,res) => {
+    try {
+        // inside the function, we want to access the user's prompt
+        const prompt = req.body.prompt;
+        // pass it off to openAI api method
+        const aiResponse = await openai.createImage({
+            prompt,
+            n:1,
+            size:'1024x1024',
+        });
+        const image = aiResponse.data.data[0].url;
+        res.send({image});
+    } catch (error) {
+        console.error(error)
+        res.status(500)
+    }
+});
+
+app.listen(8080, () => console.log('make art at http://localhost:8080/jsProject1'));
--- src/main.js
+++ src/main.js
@@ -1,24 +1,37 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { json } from 'express';
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form')
+form.addEventListener('submit',async (e) => {
+  e.preventDefault();
+  showSpinner();
+  // to pass
+  const data = new FormData(form);
+  const response = await fetch('http://localhost:8080/jsProject1', {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    body:json.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
 
-setupCounter(document.querySelector('#counter'))
+  const {image} = await response.json();
+  const result = document.querySelector('#result'); //select html with id = result
+  result.innerHTML = `<img src="${image}" width="512"/>`;
+
+  hideSpinner();
+});
+
+function showSpinner() {
+  // select enable writing
+  const button = document.querySelector('button');
+  button.disabled = true;
+  button.innerHTML = 'Creating...<span class="spinner">🤔</span>';
+}
+function hideSpinner() {
+  const button = document.querySelector('button');
+  button.disabled = false;
+
+}
